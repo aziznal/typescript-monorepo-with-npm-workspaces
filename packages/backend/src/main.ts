34ca5e46521020f27857d939beaf904def94e62f
Aziz <aziznal.dev@gmail.com>
2023-07-16 15:02:38 +0300

    update module resolution config for fancy-logger lib

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { FancyLogger } from 'fancy-logger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  FancyLogger.log('Backend has started');
 }
 bootstrap();
